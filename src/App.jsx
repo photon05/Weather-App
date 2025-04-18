15d793f63c8d8fde0233587b1994fbf3fcb00e41
photon05 <arjunbhardwaj.ab.2003@gmail.com>
2025-04-18 12:48:20 +0530

    photon/testchanges-changed the color theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,16 @@
-import { useState } from 'react'
-import './App.css'
-import Weather from './components/weather'
+import { useState } from "react";
+import "./App.css";
+import Weather from "./components/weather";
 
 function App() {
-
   return (
-    <div className='App'>
-      <h1 className='heading'>This is the Weather App</h1>
-      <Weather/>
+    <div className="App">
+      <div className="heading">
+        <h1>This is the Weather App</h1>
+      </div>
+      <Weather />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
